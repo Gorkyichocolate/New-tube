b8a92061b8be678ac4026938fbe06e4de42da844
Gorkiy Chocolate <beybagame@gmail.com>
2025-09-14 17:38:54 +0500

    123

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import { Button } from "@/components/ui/button"
+import Image from "next/image"
 
 export default function Home() {
   return(
     <div>
-      <Button> 
-        Click Me
-      </Button>
+      <Image src="/logo.svg" height={50} width={50} alt="Logo" />
+      <p className="text-xl font-semibold tracking-tight">NewTube</p>
     </div>
   )
 }
-//
